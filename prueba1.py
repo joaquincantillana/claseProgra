f9f2c45a26c76259606275719e1fe4e06d306856
joaquincantillana <joaq.cantillana@duocuc.cl>
2026-06-17 15:41:20 -0400

    practica2

--- prueba1.py
+++ prueba1.py
@@ -1,3 +1,17 @@
+def validar_nombre(nombre):
+
+
+    return nombre.strip() != ""
+ 
+def validar_especie(especie):
+
+    especies_validas = ["perro","gato","ave"]
+    return especie.strip().lower() in especies_validas
+
+def validar_edad(edad):
+    
+    return edad.isdigit() and int(edad) > 0
+
 def mostrar_menu():
     print("------MENU PRINCIPAL---------------")
     print("1.- Agregar mascota")
@@ -11,7 +25,7 @@ def mostrar_menu():
 def ingresar_opcion():
     while True:
         try:
-            opcion = int(input("Seleccione una opcion"))
+            opcion = int(input("Seleccione una opcion: "))
             if opcion < 1 or opcion > 6:
                 print("Debe de ingresar un numero del 1 al 6")
             else:
@@ -20,12 +34,54 @@ def ingresar_opcion():
             print("Debe de ingresar un numero")
     return opcion
 
+def agregar_mascota(lista):
+    nombre = input("Ingrese el nombre de la mascota: ")
+    correcto = validar_nombre(nombre)
+    if not correcto:
+        print("El nombre no puede estar vacio")
+        return
+
+    especie = input("Ingrese la especie de la mascota (perro, gato o ave): ")
+    correcto = validar_especie(especie)
+    if not correcto:
+        print("La especie solo puede ser perro, gato o ave")
+        return
+
+    edad = input("Ingrese la edad de la mascota: ")
+    correcto = validar_especie(especie)
+    if not correcto:
+        print("La edad debe de ser un numero entero mayor a cero")
+        return
+
+    mascota = {
+        "nombre": nombre.strip(),
+        "especie": especie.strip().lower(),
+        "edad": int(edad),
+        "vacunada": False
+        }
+    
+
+
 lista_mascotas = []
+
 op = 0
 while op != 6:
     mostrar_menu()
     op = ingresar_opcion()
+    if op == 1:
+        agregar_mascota(lista_mascotas)
 
+    elif op == 2:
+        print()
+    elif op == 3:
+        print()
+    elif op == 4:
+        print()
+    elif op == 5:
+        print()
+    elif op == 6:
+        print("Gracias por usar el sistema")
+    
 
 
 
